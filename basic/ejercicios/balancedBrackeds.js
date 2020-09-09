c93fd1a638610cfe956d0845e762a2ff98c58072
CristianPedrozo <cristianspedrozo@gmail.com>
2020-09-09 15:02:13 -0300

    Resolucion del ejercicio balancedBrackeds

--- basic/ejercicios/balancedBrackeds.js
+++ basic/ejercicios/balancedBrackeds.js
@@ -1,7 +1,73 @@
 // Complete the isBalanced function below.
 function isBalanced(s) {
+    var array = s.split('')
+    for (let index = 0; index < array.length; index++) {
+        //Inicializo las variables a utilizar
+        let item = array[index];
+        let itemAnt = "";
+        let itemSig = "";
+        //Compruebo que haya datos en la posicion anterior y posterior
+        if (index + 1 < array.length)
+            itemsig = array[index + 1];
+        if (index > 0)
+            itemAnt = array[index - 1]
 
+        //Dependiendo el signo se evalua cual es el siguiente requerido
+        switch (item) {
+            case '{': {
+                if (itemSig == '}') {
+                    array.splice(index, 2);
+                    index = 0;
+                }
+                break;
+            }
+            case '(': {
+                if (itemSig == ')') {
+                    array.splice(index, 2);
+                    index = 0;
+                }
+                break;
+            }
+            case '[': {
+                if (itemSig == ']') {
+                    array.splice(index, 2);
+                    index = 0;
+                }
+                break;
+            }
+            case '}': {
+                if (itemAnt == '{') {
+                    array.splice(index - 1, 2);
+                    index = 0;
+                }
+                break;
+            }
+            case ')': {
+                if (itemAnt == '(') {
+                    array.splice(index - 1, 2);
+                    index = 0;
+                }
+                break;
+            }
+            case ']': {
+                if (itemAnt == '[') {
+                    array.splice(index - 1, 2);
+                    index = 0;
+                }
 
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+
+    }
+    //Si al final del proceso el array esta vacio, esta balanceado
+    if (array.length > 0)
+        return 'NO'
+
+    return 'YES'
 }
 
 // TESTS
